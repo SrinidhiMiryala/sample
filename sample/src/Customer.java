0e66d21f7c75e717eb9ec7d27f6c1b6f3685dc22
SrinidhiMiryala <srinidhimiryala13@gmail.com>
2022-07-17 19:54:00 -0400

    methods example
    bank and customer

---
+++ sample/src/Customer.java
@@ -0,0 +1,21 @@
+class Bank{
+	void form() {
+		System.out.println("1.Withdrawal 2.Deposit");
+	}
+	void submit(String form) {
+		System.out.println("form given is "+form);
+	}
+	
+	String withdrawal(int amount) {
+		System.out.println("Amount given is "+amount);
+		return "Withdrawn";
+	}
+}
+public class Customer {
+	public static void main(String[] args) {
+	Bank nidhi=new Bank();
+	nidhi.form();
+	nidhi.submit("Withdrawal");
+	System.out.println("Acknowledgement " +nidhi.withdrawal(1000));
+	}
+}
