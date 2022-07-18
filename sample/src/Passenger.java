06868817ce0d701f6e42067fccf825c7b3a4c22c
SrinidhiMiryala <srinidhimiryala13@gmail.com>
2022-07-17 20:11:19 -0400

    method example
    metroticketcounter and passenger

---
+++ sample/src/Passenger.java
@@ -0,0 +1,23 @@
+class MetroTicketCounter{
+	void tickets() {
+		System.out.println("1.Kukatpally 2.Jntuh 3.Hitech city");
+	}
+	void destination(String destination) {
+		System.out.println("destination choosen is "+destination);
+	}
+	
+	String ticketCost(int Cost) {
+		System.out.println("Cost for ticket is " +Cost);
+		return " ";
+	}
+}
+public class Passenger {
+	public static void main(String[] args) {
+	MetroTicketCounter nidhi= new MetroTicketCounter();
+	nidhi.tickets();
+	nidhi.destination("Kukatpally");
+	System.out.println("ticket provided " +nidhi.ticketCost(50));
+	}
+}
+
+
