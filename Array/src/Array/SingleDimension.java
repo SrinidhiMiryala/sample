0b80252907da3f8b0df2f75110fe335d7e117006
SrinidhiMiryala <srinidhimiryala13@gmail.com>
2022-07-25 21:23:51 -0400

    Array
    Single and TwoDimension

---
+++ Array/src/Array/SingleDimension.java
@@ -0,0 +1,17 @@
+package Array;
+import java.util.Scanner;
+
+public class SingleDimension {
+		public static void main(String[] args) {
+			int a[]=new int[10];
+			Scanner sc=new Scanner(System.in);
+			for(int i=0;i<10;i++) {
+				System.out.println("Enter the array values");
+				a[i]=sc.nextInt();
+				
+			}
+			for(int i=0; i<10;i++) {
+			System.out.println("a["+i+"]="+a[i]);
+			}
+			}
+	}
---
+++ Array/src/Array/TwoDimension.java
@@ -0,0 +1,30 @@
+package Array;
+import java.util.Scanner;
+
+public class TwoDimension {
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			
+				System.out.println("Enter the x array values");
+				int x =sc.nextInt();
+					System.out.println("Enter the y array values");
+					int y=sc.nextInt();
+					int a[][]=new int[x][y];
+					System.out.println("Enter the x*y array values to store:\n");
+					  for (int i = 0; i < x; i++)
+						{
+						    for(int j = 0; j < y; j++)
+						    {
+					           	a[i][j] = sc.nextInt();
+						    }
+						}   
+					        System.out.print("Elements in Array are :\n");
+					        for (int i = 0; i < x; i++)
+						{
+						    for(int j = 0; j < y; j++)
+						    {
+						       System.out.println("Row ["+i+"]:  Column ["+j+"] :"+a[i][j]);
+					 	    }
+						}  
+					   }
+					}
